b5d96d04d70278f6919b23d94d4803242dfccd5e
AndrwYan <beyond1997110@gmail.com>
2023-08-16 10:18:43 +0800

    update DeveloperDetailPage component.

--- src/pages/DeveloperDetailPage.tsx
+++ src/pages/DeveloperDetailPage.tsx
@@ -1,15 +1,19 @@
 import {
+  Badge,
   Box,
   GridItem,
+  Heading,
   SimpleGrid,
-  Spinner
+  Spinner,
+  Wrap,
+  WrapItem
 } from '@chakra-ui/react';
 
 import { useParams } from 'react-router-dom';
 import useDeveloper from '../hooks/useDeveloper';
 import SingleProfileCard from '../components/SingleProfileCard';
 
-const DeveloperDetailPage = () => {
+const DeveloperDetailPage = () => {  
   
   const { slug } = useParams();
   const { data: profile, isLoading, error } = useDeveloper(slug!);
@@ -17,14 +21,44 @@ const DeveloperDetailPage = () => {
   if (isLoading) return <Spinner />;
 
   if (error || !profile) throw error;
-  
+
   return (
     <Box marginTop="1px" marginLeft="100px">
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
         <GridItem>
           <SingleProfileCard profile={profile}/>
         </GridItem>
-        <GridItem>          
+        <GridItem marginLeft="-24">
+          <Box>
+              <Heading size="md" mb="2">
+              About Me
+              </Heading>
+              
+              <Heading size="md" mb="2">
+                Top Skills
+              </Heading>
+              <Wrap mb="4">
+                {profile.top_skills.map((skill, index) => (
+                  <WrapItem key={index}>
+                    <Badge colorScheme="blue" variant="solid" mr="2" mb="2">
+                      {skill}
+                    </Badge>
+                  </WrapItem>
+                ))}
+              </Wrap>
+              <Heading size="md" mb="2">
+                Other Skills
+              </Heading>
+              <Wrap>
+                {profile.other_skills.map((skill, index) => (
+                  <WrapItem key={index}>
+                    <Badge colorScheme="green" variant="solid" mr="2" mb="2">
+                      {skill}
+                    </Badge>
+                  </WrapItem>
+                ))}
+              </Wrap>
+          </Box>
         </GridItem>
       </SimpleGrid>
     </Box>  
